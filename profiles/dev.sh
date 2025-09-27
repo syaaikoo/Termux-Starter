091d2096843349f48c4f3b2a03ffa4b629e95b28
Airaa Cheisyaa <nationa970@gmail.com>
2025-09-28 03:54:52 +0700

    Add files via upload

---
+++ profiles/dev.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# profile dev: aliases & tools
+export EDITOR=vim
+alias gs='git status'
---
+++ profiles/lite.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+# profile lite: minimal aliases
+alias ll='ls -la'
